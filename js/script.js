aece4b4830be914966e0cbf905c6663ff7584d23
Boris Batanov <majorgucsky@gmail.com>
2022-11-13 22:13:55 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ document.querySelector('.modal-close-button').addEventListener('click', () => {
   document.querySelector('.modal-container').classList.add('modal-container-close');
 });
 
-document.querySelector('.navigation-button').addEventListener('click', () => {
+document.querySelector('.modal-actived').addEventListener('click', () => {
   document.querySelector('.modal-container').classList.remove('modal-container-close');
 });
